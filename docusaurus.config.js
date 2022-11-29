12f33b20a09069e7d37b0f19bbce32bd1190d271
dvd <dvd@nc.social>
2022-11-29 16:23:13 -0500

    initial scaffolding

---
+++ docusaurus.config.js
@@ -0,0 +1,121 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'nc.social',
+  tagline: 'tagline',
+  url: 'https://about.nc.social',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ncsocial', // Usually your GitHub org/user name.
+  projectName: 'about', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ncsocial/about/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false, 
+        //{
+          //showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl:
+           // 'https://github.com/ncsocial/about/tree/main/packages/create-docusaurus/templates/shared/',
+        //},
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'nc.social',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/cardinal.jpg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'welcome',
+            position: 'left',
+            label: 'Welcome',
+          },
+          //{to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/ncsocial/about',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Home',
+                to: '/docs/welcome',
+              },
+            ],
+          },
+          {
+            title: 'Links',
+            items: [
+              {
+                label: 'nc.social',
+                href: 'https://nc.social',
+              },
+              {
+                label: 'Server Status',
+                href: 'https://status.nc.social',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/ncsocial',
+              },
+            ],
+          },
+        ],
+        copyright: `CC-BY ${new Date().getFullYear()} nc.social.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
