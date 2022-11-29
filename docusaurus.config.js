c061fad9823ca8702c460eb31ae6a85ddcbafd5c
dvd <dvd@nc.social>
2022-11-29 17:41:45 -0500

    fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
             items: [
               {
                 label: 'Home',
-                to: '/docs/welcome',
+                to: '/',
               },
             ],
           },
